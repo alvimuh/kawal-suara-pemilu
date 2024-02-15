9cf959ccc7ab030f53a306591f7f53a1827d2346
alvimuh <mohamadalvin@gmail.com>
2024-02-15 23:18:34 +0700

    feat(insight): Init insight page

--- app/page.tsx
+++ app/page.tsx
@@ -1,220 +1,35 @@
-"use client";
-
 import { createClient } from "@/utils/supabase/client";
-import { Button, Menu, Table, Tag, Tooltip } from "antd";
+import { Button, Col, Menu, Row, Statistic, Table, Tag, Tooltip } from "antd";
+import Card from "antd/es/card/Card";
 import Layout, { Content, Footer, Header } from "antd/es/layout/layout";
+import { valueType } from "antd/es/statistic/utils";
 import { ColumnsType } from "antd/es/table";
+import Link from "antd/es/typography/Link";
+import Paragraph from "antd/es/typography/Paragraph";
 import Title from "antd/es/typography/Title";
 
-interface User {
-  code: string;
-  provinsi: string;
-  kabupaten: string;
-  kecamatan: string;
-  tps: string;
-  total_votes_01: number;
-  total_votes_02: number;
-  total_votes_03: number;
-  total_votes: number;
-  total_valid_votes: number;
-  total_invalid_votes: number;
-  is_match: boolean;
-  link: string;
+interface KpuTpsError_1 {
+  jumlah_sama: valueType;
+  jumlah_tidak_sama: valueType;
 }
-
-const columns: ColumnsType<User> = [
-  {
-    key: "code",
-    title: () => (
-      <Tooltip title="Dari web KPU" placement="bottom">
-        Kode TPS
-      </Tooltip>
-    ),
-    dataIndex: "code",
-    fixed: "left",
-    width: 150,
-  },
-  {
-    key: "provinsi",
-    title: () => (
-      <Tooltip title="Dari web KPU" placement="bottom">
-        Provinsi
-      </Tooltip>
-    ),
-    dataIndex: "provinsi",
-    width: 140,
-  },
-  {
-    key: "kabupaten",
-    title: () => (
-      <Tooltip title="Dari web KPU" placement="bottom">
-        Kabupaten
-      </Tooltip>
-    ),
-    dataIndex: "kabupaten",
-    width: 140,
-  },
-  {
-    key: "kecamatan",
-    title: () => (
-      <Tooltip title="Dari web KPU" placement="bottom">
-        Kecamatan
-      </Tooltip>
-    ),
-    dataIndex: "kecamatan",
-    width: 140,
-  },
-
-  {
-    key: "tps",
-    title: () => (
-      <Tooltip title="Dari web KPU" placement="bottom">
-        Nama TPS
-      </Tooltip>
-    ),
-    dataIndex: "tps",
-    width: 140,
-  },
-  {
-    key: "total_votes_01",
-    title: () => (
-      <Tooltip
-        title="Dari web KPU pada section: Perolehan Suara"
-        placement="bottom"
-      >
-        Jumlah Suara Paslon 01
-      </Tooltip>
-    ),
-    dataIndex: "total_votes_01",
-    width: 110,
-  },
-  {
-    key: "total_votes_02",
-    title: () => (
-      <Tooltip
-        title="Dari web KPU pada section: Perolehan Suara"
-        placement="bottom"
-      >
-        Jumlah Suara Paslon 02
-      </Tooltip>
-    ),
-    dataIndex: "total_votes_02",
-    width: 110,
-  },
-  {
-    key: "total_votes_03",
-    title: () => (
-      <Tooltip
-        title="Dari web KPU pada section: Perolehan Suara"
-        placement="bottom"
-      >
-        Jumlah Suara Paslon 03
-      </Tooltip>
-    ),
-    dataIndex: "total_votes_03",
-    width: 110,
-  },
-  {
-    key: "total_votes",
-    title: () => (
-      <Tooltip
-        title="Dijumlahkan dari kolom Jumlah Suara Paslon 01 + 02 + 03"
-        placement="bottom"
-      >
-        Jumlah Suara Seluruh Paslon
-      </Tooltip>
-    ),
-    dataIndex: "total_votes",
-    width: 110,
-  },
-  {
-    key: "total_valid_votes",
-    title: () => (
-      <Tooltip
-        title="Dari web KPU pada section: Jumlah Suara Sah dan Tidak Sah"
-        placement="bottom"
-      >
-        Jumlah Suara Sah
-      </Tooltip>
-    ),
-    dataIndex: "total_valid_votes",
-    width: 110,
-  },
-  {
-    key: "total_invalid_votes",
-    title: () => (
-      <Tooltip
-        title="Dari web KPU pada section: jumlah suara sah dan tidak sah"
-        placement="bottom"
-      >
-        Jumlah Suara Tidak Sah
-      </Tooltip>
-    ),
-    dataIndex: "total_invalid_votes",
-    width: 115,
-  },
-  {
-    key: "is_match",
-    title: () => (
-      <Tooltip
-        title={`Hasil perbandingan antara kolom "Jumlah Suara Seluruh Paslon" dengan "Jumlah Suara Sah"`}
-        placement="bottom"
-      >
-        Keterangan
-      </Tooltip>
-    ),
-    dataIndex: "is_match",
-    fixed: "right",
-    width: 130,
-    render: (value, record) => {
-      let color = value ? "green" : "red";
-
-      return (
-        <Tooltip
-          title={
-            value
-              ? "Jumlah Surat Suara Sah dan Jumlah Suara Seluruh Paslon sama"
-              : `Jumlah Surat Suara Sah adalah ${record.total_valid_votes} sedangkan Jumlah Suara Seluruh Paslon adalah ${record.total_votes}`
-          }
-          placement="bottom"
-        >
-          <Tag color={color} key={value}>
-            {value ? "Sesuai" : "Tidak Sesuai"}
-          </Tag>
-        </Tooltip>
-      );
-    },
-  },
-  {
-    key: "link",
-    title: "Sumber",
-    dataIndex: "link",
-    width: 120,
-    render: (value) => {
-      return (
-        <Button type="primary" href={value} size="small" target="_blank">
-          Buka
-        </Button>
-      );
-    },
-  },
-];
 
 export default async function Page() {
   const supabase = createClient();
 
-  const { data } = await supabase.from("kpu_tps").select("*");
+  const { data } = await supabase.from("kpu_tps_error_1").select();
 
-  let dataArray: User[] = [];
+  const kpuTpsErrorData: KpuTpsError_1 = data !== null ? data[0] : null;
+  // let dataArray: User[] = [];
+  console.log(kpuTpsErrorData);
 
-  if (data !== null && data !== undefined) {
-    dataArray = data.map((item: User) => {
-      return {
-        ...item,
-        is_match: item.total_votes === item.total_valid_votes,
-      };
-    });
-  }
+  // if (data !== null && data !== undefined) {
+  //   dataArray = data.map((item: User) => {
+  //     return {
+  //       ...item,
+  //       is_match: item.total_votes === item.total_valid_votes,
+  //     };
+  //   });
+  // }
 
   return (
     <div>
@@ -246,17 +61,93 @@ export default async function Page() {
             style={{ flex: 1, minWidth: 0 }}
           />
         </Header>
-        <Content
-          style={{
-            position: "relative",
-          }}
-        >
-          <Table
-            columns={columns}
-            dataSource={dataArray}
-            scroll={{ x: 1500 }}
-            bordered
-          />
+        <Content style={{ padding: "64px 48px" }}>
+          <Title
+            level={2}
+            style={{
+              fontSize: "1.8rem",
+            }}
+          >
+            Tentang Kawal Suara Pemilu 2024
+          </Title>
+          <Paragraph>
+            Website ini dilatarbelakangi karena keresahan banyaknya data yang
+            menurut kami tidak valid dari website resmi KPU:{" "}
+            <Link>pemilu2024.kpu.go.id</Link>. Kami mencoba mengumuplkan data
+            dari web KPU tersebut dengan metode scraping secara berkala. Adapun
+            dari beberapa indikator yang kesalahan data yang kami temukan,
+            dibagi menjadi 2 topik:
+          </Paragraph>
+          <Paragraph></Paragraph>
+          <Title
+            level={3}
+            style={{
+              fontSize: "1.3rem",
+            }}
+          >
+            #1 Terdapat selisih antara jumlah suara sah dengan hasil penjumlahan
+            suara dari ketiga paslon
+          </Title>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Card bordered={false}>
+                <Statistic
+                  title="Valid"
+                  value={kpuTpsErrorData.jumlah_sama}
+                  // precision={2}
+                  valueStyle={{ color: "#3f8600" }}
+                  // prefix={<ArrowUpOutlined />}
+                  suffix="suara"
+                />
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card bordered={false}>
+                <Statistic
+                  title="Tidak Valid"
+                  value={kpuTpsErrorData.jumlah_tidak_sama}
+                  // precision={2}
+                  valueStyle={{ color: "#cf1322" }}
+                  // prefix={<ArrowDownOutlined />}
+                  suffix="suara"
+                />
+              </Card>
+            </Col>
+            <Col style={{ marginTop: "1rem" }}>
+              <Paragraph>
+                Data yang tidak valid adalah selisih antara jumlah suara sah
+                dengan hasil penjumlahan suara dari ketiga paslon tidak sama.
+                Misalnya, jika dijumlahkan hasil perolehan suara Paslon 01+02+03
+                adalah 300 suara, tapi pada field Jumlah Suara Sah adalah 200,
+                maka terdapat selisih 100 data.
+              </Paragraph>
+              <Button size="large" type="primary" href="/error_1">
+                Cek Data Terbaru
+              </Button>
+              <Paragraph style={{ marginTop: "0.5rem", color: "GrayText" }}>
+                Terakhir disingkronkan pada {new Date().toDateString()}
+              </Paragraph>
+            </Col>
+          </Row>
+          <Title
+            level={3}
+            style={{
+              fontSize: "1.3rem",
+              marginTop: "2rem",
+            }}
+          >
+            #2 Terdapat jumlah perolehan suara yang melebihi jumlah pengguna hak
+            pilih TPS
+          </Title>
+          <Row gutter={16}>
+            <Col span={24}>
+              <Card bordered={false}>
+                <Paragraph style={{ margin: "1rem" }}>
+                  Data masih kami siapkan
+                </Paragraph>
+              </Card>
+            </Col>
+          </Row>
         </Content>
         <Footer style={{ textAlign: "center" }}>
           Ant Design ©{new Date().getFullYear()} Created by Ant UED
