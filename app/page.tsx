079a978c9c01c708c2400b8423b6bb276c9f9d55
alvimuh <mohamadalvin@gmail.com>
2024-02-15 21:06:21 +0700

    feat(tps): move to root path

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,267 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/SignUpUserSteps";
-import Header from "@/components/Header";
+"use client";
 
-export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
+import { createClient } from "@/utils/supabase/client";
+import { Button, Menu, Table, Tag, Tooltip } from "antd";
+import Layout, { Content, Footer, Header } from "antd/es/layout/layout";
+import { ColumnsType } from "antd/es/table";
+import Title from "antd/es/typography/Title";
 
-  const isSupabaseConnected = canInitSupabaseClient();
+interface User {
+  code: string;
+  provinsi: string;
+  kabupaten: string;
+  kecamatan: string;
+  tps: string;
+  total_votes_01: number;
+  total_votes_02: number;
+  total_votes_03: number;
+  total_votes: number;
+  total_valid_votes: number;
+  total_invalid_votes: number;
+  is_match: boolean;
+  link: string;
+}
+
+const columns: ColumnsType<User> = [
+  {
+    key: "code",
+    title: () => (
+      <Tooltip title="Dari web KPU" placement="bottom">
+        Kode TPS
+      </Tooltip>
+    ),
+    dataIndex: "code",
+    fixed: "left",
+    width: 150,
+  },
+  {
+    key: "provinsi",
+    title: () => (
+      <Tooltip title="Dari web KPU" placement="bottom">
+        Provinsi
+      </Tooltip>
+    ),
+    dataIndex: "provinsi",
+    width: 140,
+  },
+  {
+    key: "kabupaten",
+    title: () => (
+      <Tooltip title="Dari web KPU" placement="bottom">
+        Kabupaten
+      </Tooltip>
+    ),
+    dataIndex: "kabupaten",
+    width: 140,
+  },
+  {
+    key: "kecamatan",
+    title: () => (
+      <Tooltip title="Dari web KPU" placement="bottom">
+        Kecamatan
+      </Tooltip>
+    ),
+    dataIndex: "kecamatan",
+    width: 140,
+  },
+
+  {
+    key: "tps",
+    title: () => (
+      <Tooltip title="Dari web KPU" placement="bottom">
+        Nama TPS
+      </Tooltip>
+    ),
+    dataIndex: "tps",
+    width: 140,
+  },
+  {
+    key: "total_votes_01",
+    title: () => (
+      <Tooltip
+        title="Dari web KPU pada section: Perolehan Suara"
+        placement="bottom"
+      >
+        Jumlah Suara Paslon 01
+      </Tooltip>
+    ),
+    dataIndex: "total_votes_01",
+    width: 110,
+  },
+  {
+    key: "total_votes_02",
+    title: () => (
+      <Tooltip
+        title="Dari web KPU pada section: Perolehan Suara"
+        placement="bottom"
+      >
+        Jumlah Suara Paslon 02
+      </Tooltip>
+    ),
+    dataIndex: "total_votes_02",
+    width: 110,
+  },
+  {
+    key: "total_votes_03",
+    title: () => (
+      <Tooltip
+        title="Dari web KPU pada section: Perolehan Suara"
+        placement="bottom"
+      >
+        Jumlah Suara Paslon 03
+      </Tooltip>
+    ),
+    dataIndex: "total_votes_03",
+    width: 110,
+  },
+  {
+    key: "total_votes",
+    title: () => (
+      <Tooltip
+        title="Dijumlahkan dari kolom Jumlah Suara Paslon 01 + 02 + 03"
+        placement="bottom"
+      >
+        Jumlah Suara Seluruh Paslon
+      </Tooltip>
+    ),
+    dataIndex: "total_votes",
+    width: 110,
+  },
+  {
+    key: "total_valid_votes",
+    title: () => (
+      <Tooltip
+        title="Dari web KPU pada section: Jumlah Suara Sah dan Tidak Sah"
+        placement="bottom"
+      >
+        Jumlah Suara Sah
+      </Tooltip>
+    ),
+    dataIndex: "total_valid_votes",
+    width: 110,
+  },
+  {
+    key: "total_invalid_votes",
+    title: () => (
+      <Tooltip
+        title="Dari web KPU pada section: jumlah suara sah dan tidak sah"
+        placement="bottom"
+      >
+        Jumlah Suara Tidak Sah
+      </Tooltip>
+    ),
+    dataIndex: "total_invalid_votes",
+    width: 115,
+  },
+  {
+    key: "is_match",
+    title: () => (
+      <Tooltip
+        title={`Hasil perbandingan antara kolom "Jumlah Suara Seluruh Paslon" dengan "Jumlah Suara Sah"`}
+        placement="bottom"
+      >
+        Keterangan
+      </Tooltip>
+    ),
+    dataIndex: "is_match",
+    fixed: "right",
+    width: 130,
+    render: (value, record) => {
+      let color = value ? "green" : "red";
+
+      return (
+        <Tooltip
+          title={
+            value
+              ? "Jumlah Surat Suara Sah dan Jumlah Suara Seluruh Paslon sama"
+              : `Jumlah Surat Suara Sah adalah ${record.total_valid_votes} sedangkan Jumlah Suara Seluruh Paslon adalah ${record.total_votes}`
+          }
+          placement="bottom"
+        >
+          <Tag color={color} key={value}>
+            {value ? "Sesuai" : "Tidak Sesuai"}
+          </Tag>
+        </Tooltip>
+      );
+    },
+  },
+  {
+    key: "link",
+    title: "Sumber",
+    dataIndex: "link",
+    width: 120,
+    render: (value) => {
+      return (
+        <Button type="primary" href={value} size="small" target="_blank">
+          Buka
+        </Button>
+      );
+    },
+  },
+];
+
+export default async function Page() {
+  const supabase = createClient();
+
+  const { data } = await supabase.from("kpu_tps").select("*");
+
+  let dataArray: User[] = [];
+
+  if (data !== null && data !== undefined) {
+    dataArray = data.map((item: User) => {
+      return {
+        ...item,
+        is_match: item.total_votes === item.total_valid_votes,
+      };
+    });
+  }
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
+    <div>
+      <Layout>
+        <Header
+          style={{
+            position: "sticky",
+            top: 0,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Title
+            level={1}
+            style={{
+              color: "white",
+              margin: 0,
+              fontSize: "1.2rem",
+            }}
           >
-            Supabase
-          </a>
-        </p>
-      </footer>
+            Kawal Suara Pemilu
+          </Title>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["2"]}
+            // items={items}
+            style={{ flex: 1, minWidth: 0 }}
+          />
+        </Header>
+        <Content
+          style={{
+            position: "relative",
+          }}
+        >
+          <Table
+            columns={columns}
+            dataSource={dataArray}
+            scroll={{ x: 1500 }}
+            bordered
+          />
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
     </div>
   );
 }
