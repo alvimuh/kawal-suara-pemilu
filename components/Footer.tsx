65f9d68f40921629603416c50c1bb6d9ee2e1f68
alvimuh <mohamadalvin@gmail.com>
2024-02-19 03:10:25 +0700

    fix(footer): update props type

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,15 @@ import { Typography } from "antd";
 import { Footer as FooterAntd } from "antd/es/layout/layout";
 import dayjs from "dayjs";
 
-export function Footer({ lastUpdate }: { lastUpdate: string }) {
+export function Footer({ lastUpdate }: { lastUpdate: number | null }) {
   return (
     <FooterAntd style={{ textAlign: "center", background: "white" }}>
-      <Typography.Paragraph style={{ color: "GrayText" }}>
-        Terakhir disinkronkan pada{" "}
-        {dayjs(lastUpdate).format("HH:MM DD/MM/YYYY")}
-      </Typography.Paragraph>
+      {lastUpdate && (
+        <Typography.Paragraph style={{ color: "GrayText" }}>
+          Terakhir disinkronkan pada{" "}
+          {dayjs(lastUpdate).format("HH:MM DD/MM/YYYY")}
+        </Typography.Paragraph>
+      )}
       Alvilab ©{new Date().getFullYear()} Pemilu Damai
     </FooterAntd>
   );
