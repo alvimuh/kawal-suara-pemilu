5f27cc41d62bc534f310b79cd348ca7fdd34c2fc
alvimuh <mohamadalvin@gmail.com>
2024-02-19 11:42:48 +0700

    fix(data-error-1): remove use client

--- app/data-error-1/page.tsx
+++ app/data-error-1/page.tsx
@@ -1,6 +1,8 @@
 import Layout from "@/components/Layout";
 import MainContent from "./components/MainContent";
 
+export const revalidate = 0;
+
 export default function Page({}) {
   return (
     <Layout contentStyle={{ padding: 0 }}>
