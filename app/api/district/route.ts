d85e42537a724fc3c5f6a4edd42ef525644a7ddb
alvimuh <mohamadalvin@gmail.com>
2024-02-22 09:36:52 +0700

    refactor(district): using new db

--- app/api/district/route.ts
+++ app/api/district/route.ts
@@ -1,6 +1,7 @@
-import { District, Insight } from "@/lib/types";
+import { District } from "@/lib/types";
 import { createClient } from "@/utils/supabase/client";
 import { NextRequest } from "next/server";
+import prisma from "@/prisma/db";
 
 export const revalidate = 0;
 
@@ -14,53 +15,28 @@ export async function GET(request: NextRequest) {
 
   switch (request.nextUrl.searchParams.get("type")) {
     case "provinsi":
-      const { data: provinsiData } = await supabase
-        .from("kpu_provinsi")
-        .select();
+      data = await prisma.$queryRaw`SELECT * FROM "kpu_provinsi"`;
 
-      if (provinsiData !== null && provinsiData !== undefined) {
-        data = provinsiData;
-      }
       break;
 
     case "kabupaten":
-      const kabupatenQuery = supabase.from("kpu_kabupaten").select();
-      kabupatenQuery.eq("provinsi", provinsi);
+      data =
+        await prisma.$queryRaw`SELECT * FROM "kpu_kabupaten" WHERE provinsi = ${provinsi}`;
 
-      const { data: kabupatenData } = await kabupatenQuery;
-
-      if (kabupatenData !== null && kabupatenData !== undefined) {
-        data = kabupatenData;
-      }
       break;
 
     case "kecamatan":
-      const kecamatanQuery = supabase.from("kpu_kecamatan").select();
-      kecamatanQuery.eq("provinsi", provinsi);
-      kecamatanQuery.eq("kabupaten", kabupaten);
+      data =
+        await prisma.$queryRaw`SELECT * FROM "kpu_kecamatan" WHERE provinsi = ${provinsi} AND kabupaten = ${kabupaten}`;
 
-      const { data: kecamatanData } = await kecamatanQuery;
-
-      if (kecamatanData !== null && kecamatanData !== undefined) {
-        data = kecamatanData;
-      }
       break;
 
     case "kelurahan":
-      const kelurahanQuery = supabase.from("kpu_kelurahan").select();
-      kelurahanQuery.eq("provinsi", provinsi);
-      kelurahanQuery.eq("kabupaten", kabupaten);
-      kelurahanQuery.eq("kecamatan", kecamatan);
+      data =
+        await prisma.$queryRaw`SELECT * FROM "kpu_kelurahan" WHERE provinsi = ${provinsi} AND kabupaten = ${kabupaten} AND kecamatan = ${kecamatan}`;
 
-      const { data: kelurahanData } = await kelurahanQuery;
-
-      if (kelurahanData !== null && kelurahanData !== undefined) {
-        data = kelurahanData;
-      }
       break;
   }
 
   return Response.json({ data });
-
-  //   return Response.json({ data });
 }
